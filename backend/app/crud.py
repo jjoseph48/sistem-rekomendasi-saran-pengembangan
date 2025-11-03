3c2704a74a485733ae1f828b13b9c40825077239
jjoseph48 <josephdiva2@gmail.com>
2025-11-03 10:50:11 +0700

    add comment

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -1,10 +1,17 @@
 from sqlalchemy.orm import Session
 from . import models, schemas
 
+# crud.py di proyek FastAPI ini berfungsi sebagai lapisan logika database di antara router dan model
+
 def get_or_create_pegawai(db: Session, nip: str, data: schemas.PegawaiCreate):
+    # mengecek apakah pegawai dengan nip tertentu sudah ada
+    # kalau belum ada, otomatis buat baru
+    # mengembalikan instance Pegawai
     pegawai = db.query(models.Pegawai).filter(models.Pegawai.nip == nip).first()
     if pegawai:
         return pegawai
+    # saat membuat pegawai baru, field kompetensi akan ikut dikirim di data.dict()
+    # tapi models.Pegawai tidak punyak kolom bernama kompetensi - hanya relasi ORM
     new_pegawai = models.Pegawai(**data.dict())
     db.add(new_pegawai)
     db.commit()
@@ -12,6 +19,7 @@ def get_or_create_pegawai(db: Session, nip: str, data: schemas.PegawaiCreate):
     return new_pegawai
 
 def create_saran(db: Session, pegawai_id: int, saran: str):
+    # membuat entri baru di tabel saran_pengembangan
     new_saran = models.SaranPengembangan(pegawai_id=pegawai_id, saran_pengembangan=saran)
     db.add(new_saran)
     db.commit()
